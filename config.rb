8d2e5f51c277b78b8f116939b4b0be5b6f99328f
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 12:24:01 +0100

    Separate gemspec

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'rake'
+
+PKG_NAME = 'malloc'
+PKG_VERSION = '1.5.1'
+SUMMARY = %q{Object for raw memory allocation and pointer operations}
+DESCRIPTION = %q{This Ruby extension defines the class Hornetseye::Malloc. Hornetseye::Malloc#new allows you to allocate memory, using Hornetseye::Malloc#+ one can do pointer manipulation, and Hornetseye::Malloc#read and Hornetseye::Malloc#write provide reading Ruby strings from memory and writing Ruby strings to memory.}
+LICENSE = 'GPL-3+'
+AUTHOR = %q{Jan Wedekind}
+EMAIL = %q{jan@wedesoft.de}
+HOMEPAGE = %q{http://wedesoft.github.com/malloc/}
+CFG = RbConfig::CONFIG
+CXX = ENV[ 'CXX' ] || 'g++'
+RB_FILES = FileList[ 'config.rb', 'lib/**/*.rb' ]
+CC_FILES = FileList[ 'ext/*.cc' ]
+HH_FILES = FileList[ 'ext/*.hh' ] + FileList[ 'ext/*.tcc' ]
+TC_FILES = FileList[ 'test/tc_*.rb' ]
+TS_FILES = FileList[ 'test/ts_*.rb' ]
+SO_FILE = "ext/#{PKG_NAME.tr '\-', '_'}.#{CFG[ 'DLEXT' ]}"
+PKG_FILES = [ 'Rakefile', 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + CC_FILES + HH_FILES + TS_FILES + TC_FILES
---
+++ malloc.gemspec
@@ -0,0 +1,22 @@
+require_relative 'config'
+
+Gem::Specification.new do |s|
+  s.name = PKG_NAME
+  s.version = PKG_VERSION
+  s.platform = Gem::Platform::RUBY
+  s.date = Date.today.to_s
+  s.summary = SUMMARY
+  s.description = DESCRIPTION
+  s.license = LICENSE
+  s.author = AUTHOR
+  s.email = EMAIL
+  s.homepage = HOMEPAGE
+  s.files = PKG_FILES
+  s.test_files = TC_FILES
+  s.require_paths = [ 'lib', 'ext' ]
+  s.rubyforge_project = %q{hornetseye}
+  s.extensions = %w{Rakefile}
+  s.has_rdoc = 'yard'
+  s.extra_rdoc_files = []
+end
+
